e705385fac13b5fc2bd874542729e1b5371032ec
dmike16 <cipmiky@gmail.com>
2019-05-05 23:42:00 +0200

    build: create the info task that display package name

---
+++ scripts/info.cwd.ts
@@ -0,0 +1,11 @@
+export default async function (args: any, root: string, logger: any) {
+    logger.info("      _   _           _ _ _ _           _          \n");
+    logger.info("     | | | |                 |         | |        \n");
+    logger.info("     | | | |                 |         | |        \n");
+    logger.info("     | | | |                 |         | |        \n");
+    logger.info("     | | | |          _ _ _ _|         | |_ _ _ _ \n");
+    logger.info(" _ _ | | | |         |        \\        |         |\n");
+    logger.info("|      | | | _ _ _   |         \\       |         |\n");
+    logger.info("|_ _ _ | | _ _ _ _ | | _ _ _ _ _\\_ _   | _ _ _ _ |\n");
+    return 0;
+}
