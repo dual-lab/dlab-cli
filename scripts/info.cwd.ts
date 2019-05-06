f889aaf8c08e1853054cdf8f7861152838008c76
dmike16 <cipmiky@gmail.com>
2019-05-06 23:33:07 +0200

    build: (1/2) create package registry

--- scripts/info.cwd.ts
+++ scripts/info.cwd.ts
@@ -1,4 +1,9 @@
+const { packages } = require("../lib/packages");
+
 export default async function (args: any, root: string, logger: any) {
+    const pkgNames = Object.keys(packages);
+
+    logger.info("\t\tMonolythic Repo \n");
     logger.info("      _   _           _ _ _ _           _          \n");
     logger.info("     | | | |                 |         | |        \n");
     logger.info("     | | | |                 |         | |        \n");
@@ -7,5 +12,13 @@ export default async function (args: any, root: string, logger: any) {
     logger.info(" _ _ | | | |         |        \\        |         |\n");
     logger.info("|      | | | _ _ _   |         \\       |         |\n");
     logger.info("|_ _ _ | | _ _ _ _ | | _ _ _ _ _\\_ _   | _ _ _ _ |\n");
+
+    logger.info("                                                   \n");
+    logger.info(`\t\tPackages #${pkgNames.length}\n`);
+    pkgNames.forEach((name) => {
+        logger.info(`\t\t\t -${name}\n`);
+        logger.info(`\t\t\t   *${packages[name]["description"]} \n`);
+    });
+    
     return 0;
 }
